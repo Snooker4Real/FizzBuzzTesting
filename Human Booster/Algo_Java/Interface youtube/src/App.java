be62d62956dea88538dc1b5804e0bfaf66cd9f8e
Cindano Jonathan <cindanojonathan@icloud.com>
2021-07-10 01:26:52 +0200

    create repository interface (exercises)

---
+++ Human Booster/Algo_Java/Interface youtube/src/App.java
@@ -0,0 +1,13 @@
+//import cindanojonathan.*;
+
+public class App {
+    public static void main(String[] args) {
+        Cottage c = new Cottage();
+        c.build("bois");
+
+        Ship s = new Ship();
+        Glider g = new Glider();
+        s.moveXY(146,97);
+        g.moveXY(80,23);
+    }
+}
---
+++ Human Booster/Algo_Java/Interface youtube/src/Vehicle.java
@@ -0,0 +1,4 @@
+abstract public class Vehicle {
+    protected int mMovementSpeed;
+    public abstract void moveXY(int x, int y);
+}
